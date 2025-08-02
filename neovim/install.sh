bd8b7f82f04e4c984849f70c9ab42e871de49cbf
Jacky161 <jacky.161@outlook.com>
2025-08-02 13:29:03 -0600

    Use ARCH variable

--- neovim/install.sh
+++ neovim/install.sh
@@ -5,8 +5,9 @@
 
 
 INSTALL_DIR="${HOME}/.local/bin"
-NVIM_DOWNLOAD="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
-#NVIM_DOWNLOAD="https://github.com/neovim/neovim-releases/releases/latest/download/nvim-linux-x86_64.tar.gz"  # For old glibc
+ARCH="x86_64"
+NVIM_DOWNLOAD="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-${ARCH}.tar.gz"
+#NVIM_DOWNLOAD="https://github.com/neovim/neovim-releases/releases/latest/download/nvim-linux-${ARCH}.tar.gz"  # For old glibc
 
 
 # Quit immediately in case any command fails
@@ -19,19 +20,19 @@ mkdir -p "${INSTALL_DIR}"
 
 # Delete old files if exists
 printf "Cleaning up old installs...\n"
-rm -rf "${INSTALL_DIR}/nvim" "${INSTALL_DIR}/nvim-linux-x86_64"
+rm -rf "${INSTALL_DIR}/nvim" "${INSTALL_DIR}/nvim-linux-${ARCH}"
 
 # Download and extract latest release
-printf "Downloading nvim-linux-x86_64.tar.gz...\n"
-curl -fLo "${INSTALL_DIR}/nvim-linux-x86_64.tar.gz" ${NVIM_DOWNLOAD}
-printf "Extracting nvim-linux-x86_64.tar.gz...\n"
-tar -xf "${INSTALL_DIR}/nvim-linux-x86_64.tar.gz" -C ${INSTALL_DIR}
+printf "Downloading nvim-linux-${ARCH}.tar.gz...\n"
+curl -fLo "${INSTALL_DIR}/nvim-linux-${ARCH}.tar.gz" ${NVIM_DOWNLOAD}
+printf "Extracting nvim-linux-${ARCH}.tar.gz...\n"
+tar -xf "${INSTALL_DIR}/nvim-linux-${ARCH}.tar.gz" -C ${INSTALL_DIR}
 
 # Delete old tar
-rm "${INSTALL_DIR}/nvim-linux-x86_64.tar.gz"
+rm "${INSTALL_DIR}/nvim-linux-${ARCH}.tar.gz"
 
 # Symlink the executable back to the install directory
-ln -s "${INSTALL_DIR}/nvim-linux-x86_64/bin/nvim" "${INSTALL_DIR}/nvim"
+ln -s "${INSTALL_DIR}/nvim-linux-${ARCH}/bin/nvim" "${INSTALL_DIR}/nvim"
 
 printf "Done! You may need to reopen your shell for neovim to be usable. Depending on your distro, you may have to add the install directory to your \$PATH variable. See the following example.\n\n"
 
